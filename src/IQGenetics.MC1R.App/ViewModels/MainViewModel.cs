19ee982b6797ec47be88cbbe0c45483ccd3cf676
Nahuel <85759476+nahuelaquiles@users.noreply.github.com>
2026-01-27 17:53:26 -0300

    Update MainViewModel.cs

--- src/IQGenetics.MC1R.App/ViewModels/MainViewModel.cs
+++ src/IQGenetics.MC1R.App/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Runtime.CompilerServices;
 using Microsoft.Win32;
 using IQGenetics.MC1R.Core.MC1R;
